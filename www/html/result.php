e438211ccf5b329b8eb95df825b3981cec37b5a2
donald-shrimp <t0706244@gmail.com>
2021-12-01 17:59:00 +0900

    awawa

--- www/html/result.php
+++ www/html/result.php
@@ -1,7 +1,8 @@
 <?php
 $count = 0;
 for($i=0;$i<10;$i+=1){
-    if($_POST['quiz_'+(string)$i]==$_POST['ans_'+(string)$i]){
+    $num = (string)$i;
+    if($_POST['quiz_'.$num]==$_POST['ans_'.(string)$i]){
         echo "第",$i+1,"問　正解";
         $count+=1;
     }else{
