dfac1887db15dc41a43c8b0d5ba3fa693dec5753
donald-shrimp <t0706244@gmail.com>
2021-12-01 17:55:45 +0900

    わ！

--- www/html/result.php
+++ www/html/result.php
@@ -1,12 +1,12 @@
 <?php
 $count = 0;
 for($i=0;$i<10;$i+=1){
-    if($_POST['quiz_'+$i]==$_POST['ans_'+$i]){
+    if($_POST['quiz_'+(string)$i]==$_POST['ans_'+(string)$i]){
         echo "第",$i+1,"問　正解";
-        $count +=1;
+        $count+=1;
     }else{
         echo "第",$i+1,"問　不正解";
     }
 }
-    echo "正答率",$count/10,"%";
+    echo "正答率",$count*10,"%";
 ?>
