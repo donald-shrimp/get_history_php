b9c67bb2e48f4113ac11476acf79d866d945c7ad
donald-shrimp <t0706244@gmail.com>
2021-12-10 16:38:50 +0900

    やったぜ。ブラックリスト機能できたぜ。

--- www/html/index.php
+++ www/html/index.php
@@ -42,7 +42,7 @@ try{
     $blackurl = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $key = 'url';
     foreach($blackurl as $key => $value){
-      echo $value['url'],'これはvalue<br>';
+      // echo $value['url'],'これはvalue<br>';
       if(preg_match('/'.$value['url'].'/',$contents['url'])){//ちゃんと正規表現で書かないと怒られるらしい
         $out_count += 1;
       }
