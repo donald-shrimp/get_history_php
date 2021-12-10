2bc626da3ea3ea39c7a986e352c140086686bde8
donald-shrimp <t0706244@gmail.com>
2021-12-10 16:12:52 +0900

    test

--- www/html/index.php
+++ www/html/index.php
@@ -43,7 +43,7 @@ try{
     $key = 'url';
     foreach($blackurl as $key => $value){
       var_dump($value);
-      echo '<br>';
+      echo 'これはvalue<br>';
       if(preg_match('/'.$value.'/',$contents['url'])){//ちゃんと正規表現で書かないと怒られるらしい
         $out_count += 1;
       }
