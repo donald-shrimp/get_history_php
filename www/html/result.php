efbddef2928c74f5bccc1ff18114d53509551832
donald-shrimp <t0706244@gmail.com>
2021-12-16 16:14:28 +0900

    外部のサーバーで適当に動かしてみる

--- www/html/result.php
+++ www/html/result.php
@@ -3,10 +3,10 @@ $count = 0;
 for($i=0;$i<10;$i+=1){
     $num = (string)$i;
     if($_POST['quiz_'.$num]==$_POST['ans_'.(string)$i]){
-        echo "第",$i+1,"問　正解<br>";
+        echo "第",$i+1,"問 正解<br>";
         $count+=1;
     }else{
-        echo "第",$i+1,"問　不正解<br>";
+        echo "第",$i+1,"問 不正解<br>";
     }
 }
     echo "正答率",$count*10,"%";
