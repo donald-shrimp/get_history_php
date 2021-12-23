e1678935cb457fe1fb923486e280bf4a9dfdeb49
donald-shrimp <t0706244@gmail.com>
2021-12-23 15:49:11 +0900

    アンケート付けた

--- www/html/result.php
+++ www/html/result.php
@@ -9,5 +9,8 @@ for($i=0;$i<10;$i+=1){
         echo "第",$i+1,"問 不正解<br>";
     }
 }
-    echo "正答率",$count*10,"%";
+    echo "正答率",$count*10,"%<br><br><br><br><br>";
+
+
+    echo "<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSck-W7ZyFcMFNuem2CiuU_36vLaXzZgTa-ZZGV64DiYRhgwQA/viewform?embedded=true' width='640' height='960' frameborder='0' marginheight='0' marginwidth='0'>読み込んでいます…</iframe>"
 ?>
