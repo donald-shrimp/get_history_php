55f8a0df9faa46bb8d37b97903dcaa5a1f75d6d0
donald-shrimp <t0706244@gmail.com>
2021-12-23 15:54:14 +0900

    hai

--- www/html/result.php
+++ www/html/result.php
@@ -12,5 +12,7 @@ for($i=0;$i<10;$i+=1){
     echo "正答率",$count*10,"%<br><br><br><br><br>";
 
 
-    echo "<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSck-W7ZyFcMFNuem2CiuU_36vLaXzZgTa-ZZGV64DiYRhgwQA/viewform?embedded=true' width='640' height='960' frameborder='0' marginheight='0' marginwidth='0'>読み込んでいます…</iframe>"
+    echo "<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSck-W7ZyFcMFNuem2CiuU_36vLaXzZgTa-ZZGV64DiYRhgwQA/viewform?embedded=true' width='640' height='960' frameborder='0' marginheight='0' marginwidth='0'>読み込んでいます…</iframe>";
+
+    echo "<a src='./user.html'>ホームに戻る</a>";
 ?>
